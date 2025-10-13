29a7c560de768a7abb7cfd158061ba38afb71d9e
NghiaNguyxn <nghia2592005@gmail.com>
2025-10-13 16:58:03 +0700

    Update Import.java

--- src/Model/Import.java
+++ src/Model/Import.java
@@ -1,4 +1,30 @@
 package Model;
 
+import java.time.LocalDateTime;
+
 public class Import {
+    private int id;
+    private final double total;
+    private final LocalDateTime ngayNhap;
+
+    public Import(int id, double total, LocalDateTime ngayNhap){
+        this.total = total;
+        this.ngayNhap = ngayNhap;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public double getTotal(){
+        return total;
+    }
+
+    public LocalDateTime getNgayNhap(){
+        return ngayNhap;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
