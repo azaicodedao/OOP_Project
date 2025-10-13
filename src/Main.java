7978cb3976bf3169d7119edf7dc9cf3a68a86ff0
NghiaNguyxn <nghia2592005@gmail.com>
2025-10-13 23:10:47 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) throws Exception{
         // Giao diện đẹp hơn (Nimbus Look and Feel)
-//        UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-//        new UI.Login_Frame();
-        //new UI.Home_Frame();
-//        UI.Home_Frame home_frame = new UI.Home_Frame();
-//        home_frame.setVisible(true);
+        UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        new UI.Home_Frame();
     }
 }
