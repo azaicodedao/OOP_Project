39a275f7aeaee8edd6b6e4e26cbae68a72fcf6cf
thtungptit <162528456+thtungptit@users.noreply.github.com>
2025-10-26 23:01:56 +0700

    Update Import_Detail.java

--- src/Model/Import_Detail.java
+++ src/Model/Import_Detail.java
@@ -21,27 +21,59 @@ public class Import_Detail {
     }
 
     // Getters & setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-    public int getId_Import() { return id_Import; }
-    public void setId_Import(int id_Import) { this.id_Import = id_Import; }
+    public int getId_Import() {
+        return id_Import;
+    }
+    public void setId_Import(int id_Import) {
+        this.id_Import = id_Import;
+    }
 
-    public int getId_Product() { return id_Product; }
-    public void setId_Product(int id_Product) { this.id_Product = id_Product; }
+    public int getId_Product() {
+        return id_Product;
+    }
+    public void setId_Product(int id_Product) {
+        this.id_Product = id_Product;
+    }
 
-    public String getTenSanPham() { return tenSanPham; }
-    public void setTenSanPham(String tenSanPham) { this.tenSanPham = tenSanPham; }
+    public String getTenSanPham() {
+        return tenSanPham;
+    }
+    public void setTenSanPham(String tenSanPham) {
+        this.tenSanPham = tenSanPham;
+    }
 
-    public int getSoluong() { return soluong; }
-    public void setSoluong(int soluong) { this.soluong = soluong; }
+    public int getSoluong() {
+        return soluong;
+    }
+    public void setSoluong(int soluong) {
+        this.soluong = soluong;
+    }
 
-    public double getGiaNhap() { return giaNhap; }
-    public void setGiaNhap(double giaNhap) { this.giaNhap = giaNhap; }
+    public double getGiaNhap() {
+        return giaNhap;
+    }
+    public void setGiaNhap(double giaNhap) {
+        this.giaNhap = giaNhap;
+    }
 
-    public double getGiaBan() { return giaBan; }
-    public void setGiaBan(double giaBan) { this.giaBan = giaBan; }
+    public double getGiaBan() {
+        return giaBan;
+    }
+    public void setGiaBan(double giaBan) {
+        this.giaBan = giaBan;
+    }
 
-    public double getThanhTien() { return thanhTien; }
-    public void setThanhTien(double thanhTien) { this.thanhTien = thanhTien; }
+    public double getThanhTien() {
+        return thanhTien;
+    }
+    public void setThanhTien(double thanhTien) {
+        this.thanhTien = thanhTien;
+    }
 }
